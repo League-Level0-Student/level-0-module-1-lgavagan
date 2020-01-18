81cf1ad09c115467514d78acb6f3edbb7ce3c548
League Student <league@iMac-12.attlocal.net>
2020-01-17 18:26:51 -0800

    i did the  badgers thing

---
+++ src/_12_badgers/badgerBoys.java
@@ -0,0 +1,21 @@
+package _12_badgers;
+
+public class badgerBoys {
+public static void main(String[] args)
+
+{
+	
+	for (int i = 0; i < 3; i++) {
+		
+	
+	for (int n = 0; n < 12; n++) {
+	System.out.println("Badger");
+	
+	}
+	for (int x = 0; x < 2; x++) {
+		System.out.println("Mushroom");
+	}
+}
+	System.out.println("Snake");
+}
+}
